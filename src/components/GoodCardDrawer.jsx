977d750f97f9a38bc2cdef639505826baa172cb3
timothyc <monkeichow2.github.com>
2023-02-09 02:26:01 +0800

    topbar and drawer

---
+++ src/components/GoodCardDrawer.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import { Divider, Drawer, IconButton, ListItem, ListItemIcon, styled, Typography, useTheme } from '@mui/material'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { BookmarkAdd, AddCard, Book, GppGood } from '@mui/icons-material'
+import { blue } from '@mui/material/colors';
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+    backgroundColor: blue[50]
+    ,
+}));
+
+const drawerWidth = '240px'
+
+const drawerItems = ["Cards", "Bookmarks", "About"]
+
+const getDrawerItemIcon = (button) => {
+    switch (button) {
+        case 'Cards': return <AddCard />
+        case 'Bookmarks': return <BookmarkAdd />
+        case 'About': return <Book />
+        default:
+            break;
+    }
+}
+
+
+export const GoodCardDrawer = ({ drawerIsOpen = null, setDrawerIsOpen = () => { } }) => {
+    const theme = useTheme()
+
+    const handleDrawerClose = () => {
+        setDrawerIsOpen(!drawerIsOpen)
+    }
+
+    const DrawContent = ({ handleDrawerClose = () => { }, showArrowBackIcon = false }) => {
+        return (
+            <>
+                <DrawerHeader>
+                    <div style={{ display: 'flex', flex: '2', justifyContent: 'center' }}>
+                        <IconButton size='small' edge='start' color='inherit' aria-label='logo' onClick={() => { window.location.assign('/') }}>
+                            <GppGood />
+                        </IconButton>
+                        <Typography variant='h6' component='div' onClick={() => { window.location.assign('/') }}>
+                            <span style={{ cursor: 'pointer', fontSize: '18px' }}>GOODCARD</span>
+                        </Typography>
+                    </div>
+                    {showArrowBackIcon ? <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton> : <></>}
+
+                </DrawerHeader>
+                <Divider />
+                {
+                    drawerItems.map((datum) => {
+                        return (
+                            <ListItem key={datum}>
+                                <ListItemIcon>{getDrawerItemIcon(datum)}</ListItemIcon>
+                                <a href={`/${datum.toLowerCase()}`}>{datum}</a>
+                            </ListItem>
+                        )
+                    })
+                }
+            </>
+        )
+    }
+
+    return (
+        <>
+            <Drawer
+                anchor='left'
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="temporary"
+                open={drawerIsOpen}
+                onClose={handleDrawerClose}>
+                <DrawContent handleDrawerClose={handleDrawerClose} showArrowBackIcon='true' />
+            </Drawer>
+            <Drawer
+                variant="permanent"
+                sx={{
+                    display: { xs: 'none', sm: 'block' },
+                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                }}
+                open>
+                <DrawContent />
+            </Drawer>
+        </>
+
+    )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box } from '@mui/system';
 import { IconButton, Toolbar, AppBar, Typography, Button, Stack } from '@mui/material'
-import { CatchingPokemon, Mail, Notifications } from '@mui/icons-material'
+import { Mail, Notifications, DarkMode, Menu, Settings, GppGood } from '@mui/icons-material'
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import Badge from '@mui/material/Badge';
-import { GppGood } from '@mui/icons-material';
-//TODO - add handler >>> open drawer when clicking profile icon
-//TODO - add handler >>> click iconButton link to that route 
+import { GoodCardDrawer } from './GoodCardDrawer';
+
 //TODO - responsive/ different layout on navbar >>> mobile, middle, web
+//TODO - search bar on navbar(topBar)
+
+//NOTE - FINISH: Drawer implementation @2023-02-08
+//               persistent drawer for wider&temp for small screen @2023-02-08
+//               linkage based on item clicked @2023-02-08
+//               click iconButton link to that route @2023-02-08
 
 const getIconButton = (button, badgeContent, badgeColor) => {
   switch (button) {
+    case 'ChangeMode': return <DarkMode />
     case 'Mails': return <Badge badgeContent={badgeContent} color={badgeColor}><Mail /></Badge>
     case 'Notifications': return <Badge badgeContent={badgeContent} color={badgeColor}><Notifications /></Badge>
     case 'AccountCircle': return <AccountCircle />
+    case 'Settings': return <Settings />
     default: break;
   }
 }
-const IconButtonBuilder = (props) => {  // aria-controls={menuId} // onClick={handleProfileMenuOpen}
-  const { size = 'large', edge = 'end', color = 'inherit', handler, badgeContent, badgeColor = 'error', button } = props
-  return <IconButton size={size} edge={edge} aria-label={props["aria-label"]} aria-haspopup={props["aria-haspopup"]} color={color}>
+const IconButtonBuilder = (props) => {
+  const { size = 'large', edge = 'end', color = 'inherit', handler, badgeContent, badgeColor = 'error', button, alias } = props
+  return <IconButton
+    size={size}
+    edge={edge}
+    aria-label={props["aria-label"]}
+    aria-haspopup={props["aria-haspopup"]}
+    color={color}
+    onClick={() => { window.location.assign(`/${alias}`) }}>
     {getIconButton(button, badgeContent, badgeColor)}
   </IconButton>
 }
 
-export const Navbar = () => {
-  const featureButtons = ["Add Cards", "Saved Cards", "About", "Mails", "Notifications", "AccountCircle"]
-  const featureButtonHandler = (button) => {
-    switch (button) {
-      case 'Add Cards':
-      case 'Saved Cards':
-      case 'About': return <Button color='inherit'>{button}</Button>
-      case 'Mails': return <IconButtonBuilder button={button} aria-label="show 4 new mails" aria-haspopup='true' badgeContent='4' />
-      case 'Notifications': return <IconButtonBuilder button={button} aria-label='show 17 new notifications' aria-haspopup='true' badgeContent='17' />
-      case 'AccountCircle': return <IconButtonBuilder button={button} aria-label="account of current user" aria-controls="primary-search-account-menu" aria-haspopup="false" />
-      default: return <Button color='inherit'>{button}</Button>
-    }
+const featureButtonHandler = (button) => {
+  switch (button) {
+    case 'ChangeMode': return <IconButtonBuilder alias='mode' button={button} color='inherit' aria-label="change mode" aria-haspopup='true' />
+    case 'Mails': return <IconButtonBuilder button={button} alias='mails' aria-label="show 4 new mails" aria-haspopup='true' badgeContent='4' />
+    case 'Notifications': return <IconButtonBuilder button={button} alias='notifications' aria-label='show 17 new notifications' aria-haspopup='true' badgeContent='17' />
+    case 'AccountCircle': return <IconButtonBuilder button={button} alias='account' aria-label="account of current user" aria-controls="primary-search-account-menu" aria-haspopup="false" />
+    case 'Settings': return <IconButtonBuilder button={button} alias='settings' color='inherit' aria-label="settings" aria-haspopup='true' />
+    default: return <Button color='inherit'>{button}</Button>
   }
+}
+
+export const Navbar = () => {
+  const featureButtons = ["ChangeMode", "Mails", "Notifications", "AccountCircle", 'Settings']
+  const drawerWidth = '240px'
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
 
   return (
-    <AppBar position='static'>
-      <Toolbar>
-        <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
-          <GppGood />
-        </IconButton>
-        <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>GOODCARD</Typography>
-        <Stack direction='row' spacing={2}>
-          {featureButtons.map((button) => <div>{featureButtonHandler(button)}</div>)}
-        </Stack>
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ display: 'flex' }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton size='large' edge='start' color='inherit' aria-label='logo' onClick={() => { setDrawerIsOpen(true) }}>
+            <Menu style={{ paddingRight: '15px' }} />
+          </IconButton>
+          <IconButton size='large' edge='start' color='inherit' aria-label='logo' onClick={() => { window.location.assign('/') }}>
+            <GppGood />
+          </IconButton>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }} onClick={() => { window.location.assign('/') }}>
+            <span style={{ cursor: 'pointer' }}>GOODCARD</span>
+          </Typography>
+          <Stack direction='row' spacing={2}>
+            {featureButtons.map((button) => <div key={button}>{featureButtonHandler(button)}</div>)}
+          </Stack>
+        </Toolbar>
+        <GoodCardDrawer drawerIsOpen={drawerIsOpen} setDrawerIsOpen={setDrawerIsOpen} />
+      </AppBar>
+    </Box >
   )
 }
