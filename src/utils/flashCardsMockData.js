a8af37a265c9b39d66201a9279125004c77e232e
javaskrskr <monkeichow2.github.com>
2023-03-07 02:47:14 +0800

    mock 50

--- src/utils/flashCardsMockData.js
+++ src/utils/flashCardsMockData.js
@@ -43,19 +43,259 @@ const FLASH_CARDS_MOCK_DATA = [
     },
     {
         uuid: 8,
-        flashcardName: 'UAT',
+        flashcardName: 'AWS',
         meaning: 'Use Acceptance Test',
         tag: ['Learned', 'Starred']
     },
     {
         uuid: 9,
-        flashcardName: 'UAT',
+        flashcardName: 'Git',
         meaning: 'Use Acceptance Test',
         tag: ['Learned', 'Starred']
     },
     {
         uuid: 10,
-        flashcardName: 'UAT',
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 11,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 12,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 13,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 14,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 15,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 16,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 17,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 18,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 19,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 20,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 21,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 22,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 23,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 24,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 25,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 26,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 27,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 28,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 29,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 30,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 31,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 32,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 33,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 34,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 35,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 36,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 37,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 38,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 39,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 40,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 41,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 42,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 43,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 44,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 46,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 47,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 48,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 49,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 50,
+        flashcardName: 'Flutter',
+        meaning: 'Use Acceptance Test',
+        tag: ['Learned', 'Starred']
+    },
+    {
+        uuid: 51,
+        flashcardName: 'Flutter51',
         meaning: 'Use Acceptance Test',
         tag: ['Learned', 'Starred']
     },
